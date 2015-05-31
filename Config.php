a504f912151cee64b9825c30ecafc3f25e0632d8
chinookproject <sevenrow2@yahoo.com>
2015-05-31 17:28:21 +0200

    Initia commit

--- Config.php
+++ Config.php
@@ -4,9 +4,9 @@ namespace Chinook\TestSuite;
 class Config
 {
     private $dir = __DIR__;
-
-    //public static $testFolders = array ( 'chinook-framework/Tests/' );
-    public static $testFolders = array ( 'C:/Users/David/Google Drive/xampp/htdocs/chinook-framework/Tests/' );
+	
+	// Web runner
+    public static $testFolders = array ( '/' );
 }
 
 ?>
